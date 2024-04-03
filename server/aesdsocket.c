bf28a3ae99e8b3cd6a5db66b4f03a864f10af448
TommasoLabieni <259650@studenti.unimore.it>
2024-04-03 02:56:21 +0200

    Implement Multithread Socket Server

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -12,31 +12,119 @@
 #include <fcntl.h>
 #include <signal.h>
 #include <sys/wait.h>
+#include <pthread.h>
+#include <list.h>
 
 #define LISTEN_BACKLOG 10 /* Max number of pending connections*/
 #define OUT_FILENAME "/var/tmp/aesdsocketdata"
 #define BUF_SIZE 128
 
 int out_fd;
-char *content = NULL;
-char *s = NULL;
-int sock_fd, cfd;
+char s[4], *content = NULL;
+int sock_fd;
+ssize_t f_length = 0, prev_length = 0;
+pthread_mutex_t mux;
+List l = NULL;
+
+void WaitThread()
+{
+    ListNode *tmp = l;
+
+    pthread_mutex_lock(&mux);
+    while (tmp != NULL)
+    {
+        if ((tmp->data->complete) && !(tmp->data->joined))
+        {
+            tmp->data->joined = true;
+            pthread_join((*tmp->data->tid), NULL);
+            close(tmp->data->cfd);
+        }
+        tmp = tmp->next;
+    }
+    pthread_mutex_unlock(&mux);
+}
 
 static void
 sig_handler_f(int sig_num)
 {
     syslog(LOG_DEBUG, "Caught signal, exiting\n");
 
-    if (content != NULL)
-        free(content);
-    if (s != NULL)
-        free(s);
+    WaitThread();
     close(out_fd);
     close(sock_fd);
-    close(cfd);
+    free(content);
+    FreeList(&l);
     closelog();
 
     exit(EXIT_SUCCESS);
+}
+
+void *handle_connection(void *arg)
+{
+    ThreadData *t_data = (ThreadData *)arg;
+    pthread_t tid = (*t_data->tid);
+    ssize_t nread;
+    char buf[BUF_SIZE];
+    bool last_char = false;
+
+    for (;;)
+    {
+        nread = recv(t_data->cfd, buf, BUF_SIZE, 0);
+
+        if (nread > 0)
+        {
+            pthread_mutex_lock(&mux);
+            f_length += nread;
+
+            content = realloc(content, f_length + 1); /* Allocating 1 B more for terminating char */
+            if (content == NULL)
+            {
+                pthread_mutex_unlock(&mux);
+                syslog(LOG_ERR, "Error while reallocating new memory. Errno message is: \n%s\n", strerror(errno));
+                exit(-1);
+            }
+
+            for (size_t i = prev_length, j = 0; j < nread; i++, j++)
+            {
+                content[i] = buf[j];
+                last_char = (buf[j] == '\n');
+            }
+
+            content[f_length] = 0;
+            prev_length = f_length;
+
+            size_t written_chars = write(out_fd, buf, nread);
+
+            if (written_chars != nread)
+            {
+                pthread_mutex_unlock(&mux);
+                syslog(LOG_ERR, "Error while writing to file. Errno message is: \n%s\n", strerror(errno));
+                exit(-1);
+            }
+
+            /* Return full content to client */
+            if (last_char)
+            {
+                if (send(t_data->cfd, content, f_length, 0) != f_length)
+                {
+                    pthread_mutex_unlock(&mux);
+                    syslog(LOG_ERR, "Error while writing back to socket. Errno message is: \n%s\n", strerror(errno));
+                    exit(-1);
+                }
+                pthread_mutex_unlock(&mux);
+            }
+
+            /* Clear buf */
+            memset(buf, 0, BUF_SIZE);
+        }
+        else
+        {
+            pthread_mutex_lock(&mux);
+            t_data->complete = true;
+            pthread_mutex_unlock(&mux);
+            return NULL;
+        }
+    }
 }
 
 void execute_server()
@@ -99,123 +187,102 @@ void execute_server()
         exit(-1);
     }
 
-    socklen_t peer_addr_size = sizeof(struct sockaddr);
-    struct sockaddr peer_addr;
-
-    cfd = accept(sock_fd, (struct sockaddr *)&peer_addr,
-                 &peer_addr_size);
-    if (cfd == -1)
-    {
-        syslog(LOG_ERR, "Error while accepting. Errno message is: \n%s\n", strerror(errno));
-        closelog();
-        close(sock_fd);
-        exit(-1);
-    }
-    s = malloc(peer_addr_size);
-
-    struct sockaddr_in *sin = (struct sockaddr_in *)&peer_addr;
-    inet_ntop(AF_INET, &(sin->sin_addr),
-              s, peer_addr_size);
-
-    syslog(LOG_DEBUG, "Accepted connection from %s\n", s);
-
-    int out_fd = open(OUT_FILENAME, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-
+    out_fd = open(OUT_FILENAME, O_WRONLY | O_CREAT | O_TRUNC, 0644);
     if (out_fd == -1)
     {
         syslog(LOG_ERR, "Error while opening out file. Errno message is: \n%s\n", strerror(errno));
-        free(s);
         closelog();
         close(sock_fd);
         exit(-1);
     }
 
-    ssize_t nread, f_length = 0, prev_length = 0;
-    char buf[BUF_SIZE];
-    bool last_char = false;
+    socklen_t peer_addr_size = sizeof(struct sockaddr);
+    struct sockaddr peer_addr;
+    int cfd;
 
     for (;;)
     {
-        nread = recv(cfd, buf, BUF_SIZE, 0);
-
-        if (nread > 0)
+        cfd = accept(sock_fd, (struct sockaddr *)&peer_addr,
+                     &peer_addr_size);
+        if (cfd == -1)
         {
-            f_length += nread;
+            syslog(LOG_ERR, "Error while accepting. Errno message is: \n%s\n", strerror(errno));
+            closelog();
+            close(sock_fd);
+            close(out_fd);
+            FreeList(&l);
+            exit(-1);
+        }
 
-            content = realloc(content, f_length + 1); /* Allocating 1 B more for terminating char */
-            if (content == NULL)
-            {
-                syslog(LOG_ERR, "Error while reallocating new memory. Errno message is: \n%s\n", strerror(errno));
-                free(s);
-                close(out_fd);
-                close(sock_fd);
-                closelog();
-                exit(-1);
-            }
+        struct sockaddr_in *sin = (struct sockaddr_in *)&peer_addr;
+        inet_ntop(AF_INET, &(sin->sin_addr),
+                  s, peer_addr_size);
 
-            for (size_t i = prev_length, j = 0; j < nread; i++, j++)
-            {
-                content[i] = buf[j];
-                last_char = (buf[j] == '\n');
-            }
+        syslog(LOG_DEBUG, "Accepted connection from %s\n", s);
 
-            content[f_length] = 0;
-            prev_length = f_length;
+        ThreadData *t_data = (ThreadData *)malloc(sizeof(ThreadData));
+        if (t_data == NULL)
+        {
+            syslog(LOG_ERR, "Error while allocating space for thread data. Errno message is: \n%s\n", strerror(errno));
+            closelog();
+            close(sock_fd);
+            close(out_fd);
+            FreeList(&l);
+            exit(-1);
+        }
 
-            write(out_fd, buf, nread);
+        t_data->tid = malloc(sizeof(pthread_t));
+        t_data->cfd = cfd;
+        t_data->complete = false;
+        t_data->joined = false;
 
-            /* Return full content to client */
-            if (last_char)
-            {
-                if (send(cfd, content, f_length, 0) != f_length)
-                {
-                    syslog(LOG_ERR, "Error while writing back to socket. Errno message is: \n%s\n", strerror(errno));
-                    free(content);
-                    free(s);
-                    close(out_fd);
-                    close(sock_fd);
-                    closelog();
-                    exit(-1);
-                    last_char = false;
-                }
-            }
+        if (t_data->tid == NULL)
+        {
+            syslog(LOG_ERR, "Error while allocating space for thread ID. Errno message is: \n%s\n", strerror(errno));
+            closelog();
+            close(sock_fd);
+            close(out_fd);
+            free(t_data);
+            FreeList(&l);
+            exit(-1);
+        }
 
-            /* Clear buf */
-            memset(buf, 0, BUF_SIZE);
+        if (pthread_create(t_data->tid, NULL, handle_connection, (void *)t_data) != 0)
+        {
+            syslog(LOG_ERR, "Error while spawning thread. Errno message is: \n%s\n", strerror(errno));
+            closelog();
+            close(sock_fd);
+            close(out_fd);
+            free(t_data->tid);
+            free(t_data);
+            FreeList(&l);
+            exit(-1);
+        }
+
+        ListNode *node = CreateNode(t_data);
+        if (node == NULL)
+        {
+            syslog(LOG_ERR, "Error while spawning thread. Errno message is: \n%s\n", strerror(errno));
+            closelog();
+            close(sock_fd);
+            close(out_fd);
+            free(t_data->tid);
+            free(t_data);
+            FreeList(&l);
+            exit(-1);
+        }
+
+        if (l == NULL)
+        {
+            l = node;
         }
         else
         {
-
-            syslog(LOG_DEBUG, "Closed connection from %s\n", s);
-            close(cfd);
-
-            cfd = accept(sock_fd, (struct sockaddr *)&peer_addr,
-                         &peer_addr_size);
-            if (cfd == -1)
-            {
-                syslog(LOG_ERR, "Error while accepting. Errno message is: \n%s\n", strerror(errno));
-                free(content);
-                free(s);
-                close(out_fd);
-                close(sock_fd);
-                closelog();
-                exit(-1);
-            }
-
-            sin = (struct sockaddr_in *)&peer_addr;
-            inet_ntop(AF_INET, &(sin->sin_addr),
-                      s, peer_addr_size);
-
-            syslog(LOG_DEBUG, "Accepted connection from %s\n", s);
+            AppendNode(&l, node);
         }
-    }
 
-    free(content);
-    free(s);
-    close(out_fd);
-    close(sock_fd);
-    close(cfd);
-    closelog();
+        WaitThread();
+    }
 }
 
 int main(int argc, char *argv[])
@@ -248,9 +315,17 @@ int main(int argc, char *argv[])
         exit(-1);
     }
 
+    if (pthread_mutex_init(&mux, NULL) != 0)
+    {
+        syslog(LOG_ERR, "Error while initializing Mutex. Errno message is: \n%s\n", strerror(errno));
+        closelog();
+        exit(-1);
+    }
+
+    pid_t sock_pid = -1;
     if (daemon_mode)
     {
-        pid_t sock_pid = fork();
+        sock_pid = fork();
 
         if (sock_pid == -1)
         {
@@ -258,18 +333,11 @@ int main(int argc, char *argv[])
             closelog();
             exit(-1);
         }
-        else if (sock_pid == 0)
-        {
-            execute_server();
-        }
-        else
-        {
-            exit(EXIT_SUCCESS);
-        }
     }
-    else
+    if ((sock_pid == 0) || (!daemon_mode))
     {
         execute_server();
     }
-    return 0;
+
+    exit(EXIT_SUCCESS);
 }
