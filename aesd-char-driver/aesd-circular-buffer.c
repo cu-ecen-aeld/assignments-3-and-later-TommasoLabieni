ed1172fda0f0557ac976d506fcc49b11d2a5be9e
TommasoLabieni <259650@studenti.unimore.it>
2024-04-20 18:09:19 +0200

    Fix include for kernel module

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -12,15 +12,18 @@
 #include <linux/string.h>
 #else
 #include <string.h>
+#include <stdio.h>
 #endif
 
 #include "aesd-circular-buffer.h"
-#include <stdio.h>
+
 void print_buffer(struct aesd_circular_buffer *buffer)
 {
-    for (uint8_t i = 0; i < AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED; i++)
+    uint8_t i = 0;
+    
+    for (; i < AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED; i++)
     {
-        printf("[%u]: %s\n", i, buffer->entry[i].buffptr);
+        /* TODO */
     }
 }
 
@@ -38,21 +41,16 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
                                                                           size_t char_offset, size_t *entry_offset_byte_rtn)
 {
     uint8_t idx = buffer->out_offs;
-    printf("------ BUFFER ------\n");
     print_buffer(buffer);
-    printf("------ END BUFFER ------\n");
 
     if (char_offset == 0)
     {
-        printf("char off is zero.\n");
         *entry_offset_byte_rtn = 0;
         return &(buffer->entry[idx]);
     }
 
     while (true)
     {
-        printf("Curr char_offset: %lu\n", char_offset);
-        printf("String is: %s Len is: %lu\n", buffer->entry[idx].buffptr, buffer->entry[idx].size);
         if (buffer->entry[idx].size <= char_offset)
         {
             char_offset -= buffer->entry[idx].size;
@@ -85,31 +83,26 @@ void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const s
      * Add element to the buffer
      */
 
-    printf("Written: %s\n", add_entry->buffptr);
     buffer->entry[buffer->in_offs] = *add_entry;
     buffer->in_offs += 1;
     buffer->in_offs %= AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
 
-    printf("IN OFFS: %d - OUT OFFS: %d\n", buffer->in_offs, buffer->out_offs);
 
     /**
      * Check if buffer is full
      */
     if (buffer->in_offs == buffer->out_offs)
     {
-        printf("BUFFER IS FULL!\n");
         buffer->full = true;
         return;
     }
 
     if (buffer->full)
     {
-        printf("Unset\n");
         buffer->full = false;
 
         buffer->out_offs += 1;
         buffer->out_offs %= AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
-        printf("NEW:::IN OFFS: %d - OUT OFFS: %d\n", buffer->in_offs, buffer->out_offs);
     }
 }
 
